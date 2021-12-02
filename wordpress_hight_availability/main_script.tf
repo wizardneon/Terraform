559f2e7f91bb0ff1eacd1459f368381393cff2c0
wizardneon <88318940+wizardneon@users.noreply.github.com>
2021-12-02 10:23:13 +0700

    специально вернул profile как было изначально
    вернул чтобы показать как было изначально, что он не был захардкожен и нужно было просто его указать в переменных

--- wordpress_hight_availability/main_script.tf
+++ wordpress_hight_availability/main_script.tf
@@ -1,6 +1,7 @@
 provider "aws" {
     region     = "${var.region}"
-    shared_credentials_file="${var.shared_credentials_file}" 
+    shared_credentials_file="${var.shared_credentials_file}"
+    profile    = "${var.profile}"
 }
 
 data "aws_availability_zones" "available" {}
